5db801e0a133cd1dee18fb7a6998166aacc41d9f
Gbemisola Kotoye <barkty@yahoo.com>
2023-09-17 00:19:26 +0100

    fix setup

--- src/tests/routes/Blog.test.ts
+++ src/tests/routes/Blog.test.ts
@@ -1,19 +1,27 @@
 import supertest from 'supertest'
 import server from '../../server'
-import { findBlog } from '../fixtures/blog'
+import { blogRecords, findBlog } from '../fixtures/blog'
+import { existingBlog } from '../setupAfterEnv'
 
 jest.mock('../../repository/Blog.service')
 
 describe('A GET request to /api/blog route', () => {
     it('returns a HTTP 200 response for a blog', async () => {
-        const result = await supertest(server).get(`/api/blog/${findBlog.id}`)
-        // expect(result.statusCode).toBe(200)
-        expect(result.body).toEqual(findBlog)
+        const result = await supertest(server).get(`/api/blog/${existingBlog.id}`)
+        expect(result.statusCode).toBe(200)
+        expect(result.body).toEqual(existingBlog)
     })
 
     it('returns a HTTP error 404 for a non-existing blog', async () => {
         const result = await supertest(server).get(`/api/blog/${findBlog.id}`)
         expect(result.notFound).toBeTruthy()
         expect(result.statusCode).toBe(404)
+    })
+
+    it('returns HTTP response 200 for all blog records', async () => {
+        const result = await supertest(server).get(`/api/blog?page=1&limit=20`)
+        console.log('BODY:: ', result.body)
+        expect(result.status).toBe(200)
+        expect(result.body).toEqual({success: 1, message: 'Successful'})
     })
 })
--- src/tests/setupAfterEnv.ts
+++ src/tests/setupAfterEnv.ts
@@ -1,6 +1,11 @@
+import Blog from '../models/Blog.entity';
+import BlogRepository from '../repository/Blog.service';
 import { AppDataSource } from '../services/database';
+import { blog } from './fixtures/blog';
 
 const dataSource = AppDataSource
+
+export let existingBlog: any
 
 beforeAll(async () => {
     await dataSource.initialize()
@@ -9,8 +14,11 @@ beforeAll(async () => {
     } else {
         console.log('Database error')
     }
+    const blogRepo = new BlogRepository()
+    existingBlog = await blogRepo.create(blog)
 })
 
 afterAll(async () => {
+    // console.log({existingBlog})
     await dataSource.destroy()
 })
